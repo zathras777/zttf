c1876477c01c0b010e43f4d8bb73e41cdce89ecb
david reid <zathrasorama@gmail.com>
2015-03-21 14:31:23 +0000

    First pass at a setup script with (hopefully) the right pieces in place. Add a version, starting at 0.1.

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from os import path
+from zttf import __version__
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='zttf',
+    version=__version__,
+    description='A TrueType font parser',
+    long_description=long_description,
+    url='https://github.com/zathras777/zttf',
+    author='david reid',
+    author_email='zathrasorama@gmail.com',
+    license='Apache20',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Fonts',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='fonts truetype ttf',
+    packages=['zttf']
+)
